cdb017654ff5acb663739aa5fcbddd71988e71d9
Asraful Islam <asrafbd2323@gmail.com>
2022-06-27 18:35:35 +0600

    files added

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello App!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello I am App 1" );
+    }
+}
